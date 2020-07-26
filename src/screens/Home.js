a912391b0ddf7630dbc616962e3fad3329cb3c26
Tony <toneyobaze@gmail.com>
2020-07-26 21:25:19 +0000

    add headerAdd icon

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,28 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-const Home = () => {
+const Home = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity style={style.headerIcon}>
+          {Platform.OS === "ios" ? (
+            <Ionicons name="ios-add" size={24} color="black" />
+          ) : (
+            <MaterialIcons name="add" size={24} color="black" />
+          )}
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={style.container}>
       <Text>Home</Text>
@@ -14,6 +35,10 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  headerIcon: {
+    padding: 16,
   },
 });
 
