79415014b4300af62d26706194cdd9e21598b762
Tony <toneyobaze@gmail.com>
2020-08-04 00:01:55 +0000

    add ViewPager

--- src/screens/CreateItem.js
+++ src/screens/CreateItem.js
@@ -1,21 +1,39 @@
-import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import React, { useState, createRef } from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
 import { useHeaderHeight } from "@react-navigation/stack";
+import ViewPager from "@react-native-community/viewpager";
 import ItemForm from "../components/ItemForm";
 
 const CreateItem = () => {
   const headerHeight = useHeaderHeight();
+  const viewPager = createRef();
+  const [currentPage, setCurrentPage] = useState(0);
 
   return (
-    <View style={[styles.container]}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingTop: headerHeight }}
+    <View style={[styles.container, { marginTop: headerHeight }]}>
+      <ViewPager
+        ref={viewPager}
+        style={styles.viewPager}
+        initialPage={0}
+        // scrollEnabled={false}
+        pageMargin={16}
+        onPageSelected={(page) => {
+          setCurrentPage(page.nativeEvent.position);
+        }}
       >
-        <View style={styles.innerContainer}>
+        <View key="1" style={styles.pages}>
           <ItemForm />
         </View>
-      </ScrollView>
+        <View key="2" style={styles.pages}></View>
+        <View key="3" style={styles.pages}></View>
+      </ViewPager>
+
+      {/* <Button
+        onPress={(e) => {
+          // viewPager.current?.setPage(1);
+        }}
+        title="Btn"
+      /> */}
     </View>
   );
 };
@@ -25,8 +43,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  innerContainer: {
-    marginHorizontal: 16,
+  viewPager: {
+    flex: 1,
+    margin: 16,
+  },
+
+  pages: {
+    backgroundColor: "#eee",
+    borderRadius: 20,
+    padding: 8,
   },
 });
 
