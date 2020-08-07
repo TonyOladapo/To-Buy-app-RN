a20a5b88058128f6182d6e47174a7debb22bfde8
Tony <toneyobaze@gmail.com>
2020-08-07 19:55:52 +0000

    refactor Header

--- Navigator.js
+++ Navigator.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { createRef } from "react";
 import { BlurView } from "expo-blur";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet } from "react-native";
+import { CommonActions } from "@react-navigation/native";
 
 import Home from "./src/screens/Home";
 import CreateItem from "./src/screens/CreateItem";
+import { CrossPlatformIcon } from "./src/components/CrossPlatform/CrossPlatformIcon";
+import Touchable from "./src/components/CrossPlatform/Touchable";
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
+  const navigationRef = createRef();
+
+  const goBack = () => {
+    navigationRef.current?.dispatch(CommonActions.goBack());
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
@@ -23,6 +32,19 @@ const Navigator = () => {
               style={[StyleSheet.absoluteFill]}
             />
           ),
+          headerLeft: ({ canGoBack }) => {
+            return (
+              canGoBack && (
+                <Touchable style={styles.ic_back} onPress={goBack}>
+                  <CrossPlatformIcon
+                    name="arrow-back"
+                    size={24}
+                    color="black"
+                  />
+                </Touchable>
+              )
+            );
+          },
           headerStyle: { elevation: 0 },
           cardStyle: { backgroundColor: "#ffffff" },
           headerTitleStyle: { fontSize: 30, fontWeight: "bold" },
@@ -40,5 +62,11 @@ const Navigator = () => {
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  ic_back: {
+    padding: 16,
+  },
+});
 
 export default Navigator;
